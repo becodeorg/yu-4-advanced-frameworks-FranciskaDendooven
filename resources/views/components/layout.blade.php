3e971f599c140695540f883fd3b3c65f66c8d59d
FranciskaDendooven <franciska.dendooven@gmail.com>
2022-08-10 12:12:26 +0200

    Initialize git repo from master to main

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<title>My first Laravel Form</title>
+<link rel="stylesheet" href="app.css">
+
+
+<body>
+    @include('header')
+
+    @yield('content')
+
+    @include('footer')
+</body>
---
+++ resources/views/header.blade.php
@@ -0,0 +1,6 @@
+<header>
+    <img src="/public/Logo_!.png" alt="">
+    <h1>My first Laravel Form</h1>
+    <p>By Franciska</p>
+    <hr>
+  </header>
