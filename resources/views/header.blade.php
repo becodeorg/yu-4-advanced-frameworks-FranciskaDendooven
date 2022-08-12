b766d78e6e0b90d6b9e72c0b10277572dc7d1b39
FranciskaDendooven <franciska.dendooven@gmail.com>
2022-08-12 14:42:34 +0200

    Wip homepage styling

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,6 +1,5 @@
 <header>
-    {{-- <img src="/public/Logo_!.png" alt="Handmade circle-like logo in black and white that represents the initials FJD with Ciska written inside the big circle"> --}}
-    <h1>My first Laravel Form</h1>
-    <p>By Franciska</p>
-    <hr>
+    {{-- <img src="/public/img/Logo_!.png" alt="Handmade circle-like logo in black and white that represents the initials FJD with Ciska written inside the big circle"> --}}
+    <h1>Your Local Butcher Shop</h1>
+    <h2>Ciska.LeSourd</h2>
   </header>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,23 @@
 @extends('components.layout')
     @section('content')
         <section class="index-home">
-            <h1 id="welcome">Welcome</h1>
+            
+            <h3>Welcome to your</h3><h3 id="local-butcher">Local Butcher</h3><h3>Shop</h3>
+            <h5>Sign up to get access to exclusive tattoo designs and book your appointment</h5>
+
                 <div class="signup">
                     <p>Don't have an account yet?<br>Sign up here!</p>
                     <br>
                     <h4><a href="{{ route('submitSignUp') }}"><button>SIGN UP</button></a></h4>
             </div>
-            <hr>
+
+            </div>
+            <br>
             <div class="login">
-                <p>Have an account? Login here!</p>
+                <p>Have an account?<br>Great!<br>Login here!</p>
                 <br>
                 <h4><a href="{{ route('form') }}"><button>LOGIN</button></a></h4>
             </div>
+
         </section>
     @endsection
