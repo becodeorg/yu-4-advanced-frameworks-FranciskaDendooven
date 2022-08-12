0cfc900d3b5c25d22dda9573de0f49d791c8e070
FranciskaDendooven <franciska.dendooven@gmail.com>
2022-08-12 16:09:33 +0200

    Finish the styling of the loginform page

--- resources/views/form-body.blade.php
+++ resources/views/form-body.blade.php
@@ -1,21 +1,25 @@
 @extends('components/layout')
 @section('content')
         <div class="index-home">
-            <p>Have an account? Login here!</p>
+        <div class="login-form">
+            <p>Welcome back! Login here!</p>
             <form action="{{ route('submitLogin') }}" method="post">
                 @csrf
                 <input type="text" name="name" value="Username">
                 @error('name')
                 <li>{{ $message }}</li>
                 @enderror
+                <br>
                 <input type="password" name="password" value="Password">
                 @error('password')
                 <li>{{ $message }}</li>
                 @enderror
                 <br>
-                <button type="submit" name="submit">LOGIN</button>
+                <button class="login-button" type="submit" name="submit">LOGIN</button>
             </form>
         </div>
-        <a href="{{ route('home') }}">Home</a>
+        <p id="add-info">No account yet? Click on the link below to go back to the homepage, where you can find the button to sign up to Your <h3 id="local-butcher-login">Local Butcher</h3></p>
+        <a id="home" href="{{ route('home') }}">Home</a>
+        </div>
     </section>
 @endsection
